7d8b626cf4799b478278972710271c67dcbd8f30
DaviAntonaji <davi.antonajis@gmail.com>
2022-01-26 12:09:10 -0300

    feat: validação de cep não encontrado

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 import json
 import requests
+continuar = True
+while continuar:
+    cep = input("Digite o CEP que deseja consultar: ")
 
-cep = input("Digite o CEP que deseja consultar: ")
+    cep = cep.replace(".", "")
+    cep = cep.replace("-", "")
 
-cep = cep.replace(".", "")
-cep = cep.replace("-", "")
+    req = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
 
-req = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+    try:
+        response = json.loads(req.content)
 
-response = json.loads(req.content)
-print("."*30)
-print("Informações de seu endereço:")
-print("CEP: ", cep)
-print("Logradouro: ", response["logradouro"])
-print("Complemento: ", response["complemento"])
-print("Cidade: ", response["localidade"])
-print("UF: ", response["uf"])
-print("IBGE: ", response["ibge"])
-print("GIA: ", response["gia"])
-print("DDD: ", response["ddd"])
-print("SIAFI: ", response["siafi"])
-print("."*30)
+        print("."*30)
+        print("Informações de seu endereço:")
+        print("CEP: ", cep)
+        print("Logradouro: ", response["logradouro"])
+        print("Complemento: ", response["complemento"])
+        print("Bairro: ", response["bairro"])
+        print("Cidade: ", response["localidade"])
+        print("UF: ", response["uf"])
+        print("IBGE: ", response["ibge"])
+        print("GIA: ", response["gia"])
+        print("DDD: ", response["ddd"])
+        print("SIAFI: ", response["siafi"])
+        print("."*30)
+    except:
+        print("CEP Não encontrado")
+    
+    continuarStr = input("Deseja continuar? (S/N) ").lower()
+    if "s" not in continuarStr:
+        continuar = False
+
+        
 
